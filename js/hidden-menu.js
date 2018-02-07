7e74926adb3522ab3648cadd4faec7717b04d340
Renat Shakhmatov <shushper@gmail.com>
2018-02-07 19:41:00 +0500

    Скрываем меню при ширине окна больше чем у планшета.

--- js/hidden-menu.js
+++ js/hidden-menu.js
@@ -3,24 +3,47 @@ const hiddenMenu = document.querySelector('#hidden-menu');
 const closeBtn = document.querySelector('#hidden-menu-close-btn');
 const links = document.getElementsByClassName('hidden-menu__nav-link');
 
-hamburgerBtn.addEventListener('click', showHiddenMenu);
-
 var menuShown = false;
 
 function showHiddenMenu() {
     menuShown = true;
 
+    //блокируем скролл страницы
+    document.body.classList.add('body_nonscroll')
+
     hiddenMenu.style = 'display: flex';
+
+    window.addEventListener("resize", onWindowResize);
 }
 
 function hideHiddenMenu() {
     menuShown = false;
 
+    //разблокируем скролл страницы
+    document.body.classList.remove('body_nonscroll')
+
     hiddenMenu.style = 'display: none';
+
+    window.removeEventListener("resize", onWindowResize);
 }
 
+function onWindowResize() {
+    console.log(window.innerWidth);
+
+    //Если пользователь растянет экран по ширине больше порогового значения для планшета - скрываем меню
+    if (window.innerWidth > 768) {
+        hideHiddenMenu()
+    }
+}
+
+//По клику на гамбургер-иконку показываем меню
+hamburgerBtn.addEventListener('click', showHiddenMenu);
+
+//По клику на любой пункт меню закрываем меню
 for (let i = 0; i < links.length; i++) {
     links[i].addEventListener('click', hideHiddenMenu);
 }
 
+//По клику на кнопку закрыть закрываем меню
 closeBtn.addEventListener('click', hideHiddenMenu);
+
